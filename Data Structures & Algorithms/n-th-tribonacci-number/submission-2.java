a0fed474f22b6fb2f55d79f9ffdd672800927665
YuriiKykot <105492974+YuriiKykot@users.noreply.github.com>
2026-05-04 17:21:29 +0200

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    HashMap<Integer, Integer> dp = new HashMap<>();
+
+    public int tribonacci(int n) {
+        if(n < 2){
+            return n > 0 ? 1 : 0;
+        }
+
+        if(dp.containsKey(n)) return dp.get(n);
+
+        dp.put(n, tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3));
+        return dp.get(n);
+    }
+}
